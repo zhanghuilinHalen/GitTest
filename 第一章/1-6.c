084f577a4ba474d7d9acd918ccdbb281906ba3a4
zhanghuilinHalen <2430956158@qq.com>
2017-12-09 23:57:41 +0800

    first homework

---
+++ 第一章/1-6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(vpid){
+    in no
+
+    printf("请输入一个整数：");
+    scanf("%d",&no);
+    printf("该整数减去6的结果是%d。\n",no-6);
+return 0;
+}
---
+++ 第一章/1-7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+    put("天\n地\n人");
+    return 0;
+}
