56d31077d8a979fbb0ca2d550b9a374b3ccce08c
zhanghuilinHalen <2430956158@qq.com>
2017-12-25 23:22:35 +0800

    12.25

---
+++ 第四章/continue.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+    int i,j;
+    for(i=1;i<=9;i++){
+        for (j=1;j<=9;j++){
+            int seki=i*j;
+            if(seki%10==4||seki/10==4){
+                printf(" ");
+                continue;
+            }
+            printf("%3d",seki);
+        }
+       putchar('\n'); 
+    }
+    return 0;
+}
